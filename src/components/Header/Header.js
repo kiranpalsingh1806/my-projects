59ae4a7437e511a5b5490ac0c623fd3ac949c94b
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-03-18 17:50:41 +0530

    Add Starter code

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <div className='header'>
+                My Projects
+            </div>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/ProjectList/ProjectList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ProjectList.css';
+
+const ProjectList = () => {
+    return (
+        <>
+            <div className='project-box'>
+                <a href="https://flamboyant-wing-4bc731.netlify.app/" target="_blank">Movie Explorer App</a>
+            </div>
+            <div className='project-box'>
+                <a href="https://eloquent-bell-458413.netlify.app/" target="_blank">Online Wallet</a>
+            </div>
+            <div className='project-box'>
+                <a href="www.google.com">Project 3 </a>
+            </div>
+        </>
+    );
+}
+
+export default ProjectList;
