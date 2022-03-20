d13df70f30a32d0a63e26ffc1348a2324a4580e2
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-03-20 13:47:33 +0530

    Add Grid System

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-import Header from './components/Header/Header';
 import ProjectList from './components/ProjectList/ProjectList';
 
 function App() {
   return (
     <>
-      <Header />
       <ProjectList />
     </>
   );
--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -4,15 +4,65 @@ import './ProjectList.css';
 const ProjectList = () => {
     return (
         <>
-            <div className='project-box'>
-                <a href="https://flamboyant-wing-4bc731.netlify.app/" target="_blank">Movie Explorer App</a>
-            </div>
-            <div className='project-box'>
-                <a href="https://eloquent-bell-458413.netlify.app/" target="_blank">Online Wallet</a>
-            </div>
-            <div className='project-box'>
-                <a href="www.google.com">Project 3 </a>
-            </div>
+            <header>
+                <h1 className="heading">My Projects</h1>
+            </header>
+            <main>
+                <section className="leading">
+                    <p className="leading-bigtext"></p>
+                    <p className="leading-text"></p>
+                </section>
+                <section className="cards">
+                    <article>
+                        <img className="article-img" src="https://www.wearethemighty.com/uploads/legacy/assets.rbl.ms/18518401/origin.jpg" alt=" " />
+                        <h1 className="article-title">
+                            Movie Explorer App
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://images.pexels.com/photos/2764669/pexels-photo-2764669.jpeg" alt=" " />
+                        <h1 className="article-title">
+                            Chatting Ground
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://www.wearethemighty.com/uploads/legacy/assets.rbl.ms/18518401/origin.jpg" alt=" " />
+                        <h1 className="article-title">
+                            Movie Explorer App
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://images.pexels.com/photos/2764669/pexels-photo-2764669.jpeg" alt=" " />
+                        <h1 className="article-title">
+                            Chatting Ground
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://www.wearethemighty.com/uploads/legacy/assets.rbl.ms/18518401/origin.jpg" alt=" " />
+                        <h1 className="article-title">
+                            Movie Explorer App
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://images.pexels.com/photos/2764669/pexels-photo-2764669.jpeg" alt=" " />
+                        <h1 className="article-title">
+                            Chatting Ground
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://www.wearethemighty.com/uploads/legacy/assets.rbl.ms/18518401/origin.jpg" alt=" " />
+                        <h1 className="article-title">
+                            Movie Explorer App
+                        </h1>
+                    </article>
+                    <article>
+                        <img className="article-img" src="https://images.pexels.com/photos/2764669/pexels-photo-2764669.jpeg" alt=" " />
+                        <h1 className="article-title">
+                            Chatting Ground
+                        </h1>
+                    </article>
+                </section>
+            </main>
         </>
     );
 }
